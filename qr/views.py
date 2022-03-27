fa615c5d2f0938bddf3e43f229bdabee5f526e33
Fuad-a11F <fuad.fuad.1984@list.ru>
2022-03-27 11:45:34 +0400

    first

---
+++ qr/views.py
@@ -0,0 +1,22 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import qrcode as code
+
+
+class CodeView(APIView):
+    def get(self, request):
+        qr = code.QRCode(
+            version=1,
+            error_correction=code.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        print(self.request.query_params.get('location'))
+        qr.add_data(self.request.query_params.get('location'))
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+
+        img.save('media/qwq1.jpg', 'JPEG')
+
+        return Response({"code": 'http://localhost:8000/media/qwq1.jpg'})
