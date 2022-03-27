802acd03eb8d7f491d0e6f1bc20185b5a58c27eb
Fuad-a11F <fuad.fuad.1984@list.ru>
2022-03-27 15:19:59 +0400

    first

--- qr/settings.py
+++ qr/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
 # Default primary key field type
