2cfea2e7fce4aec91696dc0bd05b7491244a28fc
Fuad-a11F <fuad.fuad.1984@list.ru>
2022-03-27 15:16:19 +0400

    first

--- qr/settings.py
+++ qr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(gj_5ll7g$1@9j54%w9cgqccqn5&7_$y1$bjga_v()v9pdqbf3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
